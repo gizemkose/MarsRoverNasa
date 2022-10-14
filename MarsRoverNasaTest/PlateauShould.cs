350371cc5fd1cee2f0eef69a3bf71f37ccc39537
gizemkip <34121505+gizemkose@users.noreply.github.com>
2022-10-14 22:25:13 +0300

    Update PlateauShould.cs

--- MarsRoverNasaTest/PlateauShould.cs
+++ MarsRoverNasaTest/PlateauShould.cs
@@ -8,14 +8,14 @@ namespace MarsRoverNasaTest
     public class PlateauShould
     {
         [Fact]
-        public void InitializeX()
+        public void Initialize_X_Dimension()
         {
             Plateau plateau = new Plateau("5 6");
             plateau.Initialize();
             Assert.Equal(5, plateau.xSize);           
         }
         [Fact]
-        public void InitializeY()
+        public void Initialize_Y_Dimension()
         {
             Plateau plateau = new Plateau("5 6");
             plateau.Initialize();
