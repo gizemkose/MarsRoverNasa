2b3c918ef84cd36456f61637998ae2ab57445e9b
gizemkip <34121505+gizemkose@users.noreply.github.com>
2022-10-14 22:24:18 +0300

    Update PlateauInputServiceShould.cs

--- MarsRoverNasaTest/PlateauInputServiceShould.cs
+++ MarsRoverNasaTest/PlateauInputServiceShould.cs
@@ -10,28 +10,28 @@ namespace MarsRoverNasaTest
     public class PlateauInputServiceShould
     {      
         [Fact]
-        public void InputControl()
+        public void Pass_With_CorrectInput()
         {
             PlateauInputService inputService = new PlateauInputService();
             var available = inputService.InputControl("5 5");
             Assert.True(available);
         }
         [Fact]
-        public void InputControl_null()
+        public void Fail_With_NullInput()
         {
             PlateauInputService inputService = new PlateauInputService();
             var available = inputService.InputControl("");
             Assert.False(available);
         }
         [Fact]
-        public void InputControl_NotNumber()
+        public void Fail_With_NotNumberInput()
         {
             PlateauInputService inputService = new PlateauInputService();
             var available = inputService.InputControl("LMLAAA");
             Assert.False(available);
         }
         [Fact]
-        public void InputControl_OnlyOneNumber()
+        public void Fail_With_OnlyOneNumberInput()
         {
             PlateauInputService inputService = new PlateauInputService();
             var available = inputService.InputControl("5");
